3bf4739ba2cf081fac8a9ee02fb71366123cfd09
Florian HAMEL <f.hamel64@gmail.com>
2018-05-07 15:19:21 +0200

    rails stupid coching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,26 @@
+class QuestionsController < ApplicationController
+
+  def answer
+    @question = params[:question]
+
+    if @question == "I am going to work"
+      @answer = "Great!"
+
+    elsif @question.include?"?"
+      @answer = "Silly question, get dressed and go to work!"
+
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+
+    # @question = params[:question]
+    # @answer = "GO back to work"
+  end
+
+  def ask
+  end
+
+
+end
+
+
